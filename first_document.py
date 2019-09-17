6eb4ed9cec947b7f1c34a660f870381092ac20c3
Rossbarr <43364032+Rossbarr@users.noreply.github.com>
2019-09-17 10:21:27 -0400

    Updated first_document.py
    I hate Matt because he annoys me.

--- first_document.py
+++ first_document.py
@@ -5,4 +5,24 @@ Created on Tue Sep 17 09:35:25 2019
 @author: Barrett
 """
 
-print("hello world")
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+
+Data1 = np.loadtxt("Data/9-10-19_12-6.data")
+
+Time = Data1[:,1]
+Detection = Data1[:,0]
+
+Muon = []
+for i in Detection:
+    if i < 20000:
+        Muon.append(i)
+        
+
+print(Muon)
+
+plt.hist(Muon)
+plt.figure(figsize = [10,10])
+plt.show()
