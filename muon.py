38fe36450322d281412fa92937d6343ad0bb00a7
Matthew Kowalski <kowal165@msu.edu>
2019-09-17 10:21:00 -0400

    Update muon.py
    did some coding, made a directory

--- muon.py
+++ muon.py
@@ -1,3 +1,15 @@
-import matplotlib as plot
+import matplotlib.pyplot as plt
+from scipy import stats
 
+data = []
+with open("Data/9-10-19_12-6.data","r") as f:
+	line = f.readline()
+	while line:
+		temp = line.split(" ")
+		if int(temp[0]) < 20000:
+                	data.append(int(temp[0]))
+		line = f.readline()
 
+plt.hist(data)
+plt.show()
+plt.save
