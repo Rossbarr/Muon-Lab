3441ccb5da0941678af426567c41da9dd1d8b134
Matthew Kowalski <kowal165@msu.edu>
2019-09-17 11:11:16 -0400

    Continued developing code

--- muon.py
+++ muon.py
@@ -1,8 +1,12 @@
+import sys
 import matplotlib.pyplot as plt
 from scipy import stats
+import numpy as np
+from scipy.optimize import curve_fit
+data_name = sys.argv[1]
 
 data = []
-with open("Data/9-10-19_12-6.data","r") as f:
+with open("Data/"+data_name+".data","r") as f:
 	line = f.readline()
 	while line:
 		temp = line.split(" ")
@@ -10,6 +14,38 @@ with open("Data/9-10-19_12-6.data","r") as f:
                 	data.append(int(temp[0]))
 		line = f.readline()
 
-plt.hist(data)
+def exponential_decay(x,a,b,c):
+	return a*np.exp(-b*x) + c
+
+def custom_fit(t,A_neg,tau_0,tau_c,A_pos,C):
+	return A_neg*np.exp(-t*(1/tau_0+1/tau_c))+A_pos*np.exp(-t/tau_0)
+
+counts, bin_edges, _ = plt.hist(data)
+time = []
+for i in range(len(bin_edges)-1):
+	time.append((bin_edges[i]+bin_edges[i+1])/2.)
+popt_exp, pcov_exp = curve_fit(exponential_decay, time, counts, p0=(1, 1e-6, 1))
+popt_custom, pcov_custom = curve_fit(custom_fit, time, counts, p0=(1, 1, 1, 1, 1, 1))
+
+x_fit = np.linspace(0, 17500, 1000)
+y_exp_fit = exponential_decay(x_fit, *popt_exp)
+y_custom_fit = custom_fit(x_fit, *popt_custom)
+plt.plot(x_fit,y_exp_fit,color="red",label="fit")
+plt.plot(x_fit,y_custom_fit,color="green",label="fit")
+plt.legend()
+plt.xlabel("Decay Time (ns)")
+plt.ylabel("Counts")
 plt.show()
-plt.savefig("Plots/TEMP.png")
+plt.savefig("Plots/"+data_name+".png")
+
+perr = np.sqrt(np.diag(pcov))
+print("Fitted histogram with a*exp(-b*x)+c")
+print("a = "+str(popt_exp[0])+"	Error in a: "+str(perr[0]))
+print("b = "+str(popt_exp[0])+"     Error in b: "+str(perr[1]))
+print("c = "+str(popt_exp[0])+"     Error in c: "+str(perr[2]))
+print("\nFitted histogram with A_neg*np.exp(-t*(1/tau_0+1/tau_c))+A_pos*np.exp(-t/tau_0)")
+print("A_neg = "+str(popt_exp[0])+"	Error in A_neg: "+str(perr[0]))
+print("tau_0 = "+str(popt_exp[0])+"     Error in tau_0: "+str(perr[1]))
+print("tau_c = "+str(popt_exp[0])+"     Error in tau_c: "+str(perr[2]))
+print("A_pos = "+str(popt_exp[0])+"     Error in A_pos: "+str(perr[2]))
+print("C = "+str(popt_exp[0])+"     Error in C: "+str(perr[2]))
