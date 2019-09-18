fef7c25f41d9b590dfe583a391ac16a3a0fc8549
Rossbarr <43364032+Rossbarr@users.noreply.github.com>
2019-09-18 11:53:02 -0400

    Finishing Documentation
    On pull_data.py

--- pull_data.py
+++ pull_data.py
@@ -1,21 +1,26 @@
 """
 Created on Tue Sep 17 11:41:47 2019
-Finished on Tue Sep 17  2019
+Finished on Wed Sep 17 12:00:00 2019
 
 @author: Barrett
 
-This document solely defines the pull_data function.
-This function will be called upon by other documents to pull the data.
-The data will be a 2d array containing time length of decay in the [:,0] (or 
-left) column and the time of occurance in the [:,1] (or right) column.
+This document defines the pull_data function and creates a txt file containing
+the cleaned up data from all dates of observation.
 
-Running this document will create (or replace) a txt file called data.txt.
-This file contains both arrays the function spits out (concatonated), so one 
-may examine it.
+The data will be a 2d array containing time length of decay in the [:,0] (or 
+top) row and the time of occurance in the [:,1] (or bottomw) row.
+
+The function may be called by other documents.
+This will certainly give the most updated information.
+
+It is, perhaps, faster in the long run to run this file and update data.txt and
+call the data from there.
+
+The second method is what we do for the majority of the project.
 """
 
-import numpy as np  # we use numpy for its excellent handling of data
-import glob         # we use glob for it excellent handling of files
+import numpy as np  # We use numpy for its excellent handling of data.
+import glob         # We use glob for it excellent handling of files.
 
 def pull_data():
     """
@@ -48,4 +53,7 @@ def pull_data():
     return time_length_of_decay, time_of_occurance
 
 time_length_of_decay, time_of_occurance = pull_data()
+# The above command calls the function.
+
 np.savetxt("data.txt",[time_length_of_decay,time_of_occurance])
+# The above command saves the output to a file called "data.txt".
